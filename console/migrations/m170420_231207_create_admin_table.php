0fa10656f56296937e5e9143c4cbbf7be7fab54f
Saleem Bakri <Saleem Bakri>
2017-04-21 05:07:49 +0300

    migrate admin table

---
+++ console/migrations/m170420_231207_create_admin_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `admin`.
+ */
+class m170420_231207_create_admin_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('admin', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(64)->notNull()->unique(),
+            'password_hash' => $this->string(255)->notNull(),
+            'auth_key' => $this->string(255)->notNull(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(0),
+            'updated_at' => 'timestamp on update current_timestamp',
+            'created_at' => 'timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP'
+        ]);
+
+        $this->insert('admin', array(
+         'username'=>'admin',
+         'status'=> 10,
+         'password_hash' => '$2y$13$PTmOcM4IANsbrX8K/5X2XuoK24j3G8zx5k6eXWhkkAnNtW17Vg.im',
+         'auth_key' => '1Z8N-KBqEWEIH-iD7zllqsUYqev1Q63d'
+        ));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('admin');
+    }
+}
